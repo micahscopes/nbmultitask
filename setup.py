f9dfe7e7b559e1550850e168a50a1114b4432cec
Micah Fitch <fitchmicah@gmail.com>
2017-11-17 22:40:13 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="control-panel-widgets",
+    version="0.1.0",
+    description="ipywidget controls for multitasking in Jupyter notebooks",
+    license="MIT",
+    author="Micah Fitch",
+    packages=find_packages(),
+    install_requires=[],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+    ]
+)
