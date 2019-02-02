59761ff3f96bec2c5b126da2038002c37ab4e975
Nezar Abdennur <nabdennur@gmail.com>
2019-02-02 13:11:43 -0500

    Add IPython cell magics and exception capture

--- nbmultitask.py
+++ nbmultitask.py
@@ -8,6 +8,11 @@ from contextlib import contextmanager
 from time import sleep
 import sys
 import random
+from io import StringIO
+import traceback
+import inspect
+from IPython.core.magic import Magics, magics_class, cell_magic
+
 
 from logging import Handler
 class LogToQueue(Handler):
@@ -22,8 +27,8 @@ class LogToQueue(Handler):
         return self.q.get()
     def empty(self):
         return self.q.empty()
-    def print(self,x,end='\n'):
-        self.write(str(x)+end)
+    def print(self, x, end='\n'):
+        self.write(str(x) + end)
 
 @contextmanager
 def empty_context():
@@ -40,11 +45,12 @@ def withLogAndControls(superclass):
             self.started = Event()
             self.exiting = Event()
             self.exited = Event()
+            self.errored = Event()
 
             self.start_stop_button = Button(description='start')
             self.start_stop_button.on_click(self.__toggle_start_stop__)
             self.start_stop_button.button_style = 'success'
-            
+
             self.refresh_log_button = Button(description='refresh log')
             self.refresh_log_button.on_click(lambda evt: self.refresh_log())
             self.clear_log_button = Button(description='clear log')
@@ -53,14 +59,14 @@ def withLogAndControls(superclass):
             self.watch_log_button = Button(description='watch')
             self.watch_log_button.button_style = 'primary'
             self.watch_log_button.on_click(self.__toggle_watch_log__)
-            
-            
+
+
             self.log = LogToQueue()
-            if issubclass(self.__class__,Thread): 
+            if issubclass(self.__class__,Thread):
                 kwargs['thread_print'] = self.log.print
             self.log_refresh_time = log_refresh_time
             self.watching_log = Event()
-            
+
             superclass.__init__(self,*init_args, args=args, kwargs=kwargs, **init_kwargs)
 
         def refresh_log(self):
@@ -68,6 +74,7 @@ def withLogAndControls(superclass):
 
             while self.watching_log.is_set() or not refreshed_once:
                 refreshed_once = True
+
                 if self.exited.is_set():
                     self.watching_log.clear()
                     self.__disable_buttons_after_exited__()
@@ -75,7 +82,10 @@ def withLogAndControls(superclass):
                 while not self.log.empty():
                     msg = self.log.get()
                     with self.output:
-                        print(msg,end='')
+                        if self.errored.is_set():
+                            print(msg, end='', file=sys.stderr)
+                        else:
+                            print(msg, end='')
                     if self.exited.is_set() and self.log.empty():
                         return
 
@@ -95,12 +105,12 @@ def withLogAndControls(superclass):
             self.watch_log_button.button_style = 'primary'
 
         def clear_log(self,wait=False):
-            self.output.clear_output(wait=wait)            
-            
+            self.output.clear_output(wait=wait)
+
         def show_log(self):
             display(self.output)
 
-        def start(self):            
+        def start(self):
             super().start()
             self.started.set()
             with self.output:
@@ -114,6 +124,12 @@ def withLogAndControls(superclass):
             if not self.started.is_set():
                 return
 
+            if self.errored.is_set():
+                with self.output:
+                    while not self.log.q.empty():
+                        msg = self.log.q.get()
+                        print(msg, end='', file=sys.stderr)
+
             if hasattr(self,'terminate') and self.use_terminate:
                 self.terminate()
                 with self.output:
@@ -124,14 +140,13 @@ def withLogAndControls(superclass):
                     print('letting work finish before stopping...')
 
                 self.exiting.set()
-
                 self.exited.wait()
                 self.exiting.clear()
-            
+
             sleep(0.1) # Need to wait a little bit before the Process object's status is `stopped`
             with self.output:
                 print(self)
-                
+
             self.__disable_buttons_after_exited__()
 
         def run(self):
@@ -140,23 +155,28 @@ def withLogAndControls(superclass):
                 sys.stderr = self.log
 
             fn = self._target if self._target is not None else getattr(self,'work')
-            
-            if self.loop:
-                while not self.exiting.is_set():
-                    fn(*self._args,**self._kwargs)
-            else:
-                fn(*self._args,**self._kwargs)
 
-            
+            try:
+                if self.loop:
+                    while not self.exiting.is_set():
+                        fn(*self._args,**self._kwargs)
+                else:
+                    fn(*self._args,**self._kwargs)
+            except Exception as e:
+                self.errored.set()
+                tb = StringIO()
+                traceback.print_exception(*sys.exc_info(), file=tb)
+                self.log.print(tb.getvalue())
+
             # when the work is done, signal that we are finished
             self.exited.set()
-        
+
         def __disable_buttons_after_exited__(self):
             self.start_stop_button.description = 'finished'
             self.start_stop_button.button_style = ''
             self.start_stop_button.disabled = True
             self.watch_log_button.close()
-        
+
         def __toggle_start_stop__(self,e=None):
             if not self.started.is_set() and not self.exited.is_set():
                 self.start()
@@ -164,13 +184,13 @@ def withLogAndControls(superclass):
                 self.stop()
 
         def __toggle_watch_log__(self,e=None):
-            if not self.watching_log.is_set() and not self.exited.is_set():
+            if not self.watching_log.is_set(): # and not self.exited.is_set():
                 self.watch()
             elif not self.exited.is_set():
                 self.stop_watching()
             elif self.exited.is_set():
                 self.stop_watching()
-                
+
         def show_start_stop_buttons(self):
             display(self.start_stop_button)
 
@@ -184,6 +204,61 @@ def withLogAndControls(superclass):
             self.show_log()
 
     return Processor
-    
+
 ProcessWithLogAndControls = withLogAndControls(Process)
 ThreadWithLogAndControls = withLogAndControls(Thread)
+
+
+# https://ipython.readthedocs.io/en/stable/config/custommagics.html
+# The class MUST call this class decorator at creation time
+@magics_class
+class RunAsync(Magics):
+    def get_frame(self):
+        stack = inspect.stack()
+        for finfo in stack:
+            if finfo.filename.startswith('<ipython-input'):
+                frame = finfo.frame
+                name = finfo.filename
+                break
+        else:
+            raise RuntimeError("Not called from an IPython cell")
+        return name, frame
+
+    @cell_magic
+    def thread(self, line, cell):
+
+        name, frame = self.get_frame()
+        code = compile(cell, name, 'exec')
+
+        def fn(thread_print):
+            lcls = locals()
+            lcls.update(frame.f_locals)
+            exec(code, frame.f_globals, lcls)
+
+        task = ThreadWithLogAndControls(target=fn, name=name)
+        return task.control_panel()
+
+    @cell_magic
+    def process(self, line, cell):
+
+        name, frame = self.get_frame()
+        code = compile(cell, name, 'exec')
+
+        def fn():
+            lcls = locals()
+            lcls.update(frame.f_locals)
+            exec(code, frame.f_globals, lcls)
+
+        task = ProcessWithLogAndControls(target=fn, name=name)
+        return task.control_panel()
+
+
+def load_ipython_extension(ipython):
+    """
+    Any module file that define a function named `load_ipython_extension`
+    can be loaded via `%load_ext module.path` or be configured to be
+    autoloaded by IPython at startup time.
+    """
+    # You can register the class itself without instantiating it.  IPython will
+    # call the default constructor on it.
+    ipython.register_magics(RunAsync)
